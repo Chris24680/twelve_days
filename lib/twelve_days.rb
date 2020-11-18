5c23e8c8be6ed60c922a4d9989494832af4c5cf0
Chris Moore <cmoore@yozu.co.uk>
2020-11-18 23:50:53 +0000

    finish refactor and tests

--- lib/twelve_days.rb
+++ lib/twelve_days.rb
@@ -1,75 +1,184 @@
-class TwelveDays
+class CountdownSong
+  def initialize(verse_factory: ChristmasDayFactory, max: 12, min: 1)
+    @verse_factory = verse_factory
+    @max = max
+    @min = min
+  end
+
   def song
-    verses(1, 12)
+    verses(@min, @max)
   end
 
   def verses(first, last)
-    (first..last).map{ |day| verse(day) }.join("\n")
+    (first..last).map{ |i| verse(i) }.join("\n")
   end
 
   def verse(day)
-    "On the #{ordiante(day)} day of Christmas my true love sent to me\n#{lines(day)}\n" 
+    @verse_factory.for(day).verse
   end
+end
 
-  def lines(day)
-    day.downto(1).map{ |day| line(day) }.join("\n")
-  end
-
-  def ordiante(day)
-    case day
+class ChristmasDayFactory
+  def self.for(day_number)
+    case day_number
     when 1
-      "first"
+      ChristmasDay.new(day_number: 1, gift: "partridge in a pear tree")
     when 2
-      "second"
+      ChristmasDayTwo.new(day_number: 2, gift: "turtle doves")
     when 3
-      "third"
+      ChristmasDayThree.new(day_number: 3, gift: "french hens")
     when 4 
-      "fourth"
+      ChristmasDayFour.new(day_number: 4, gift: "calling birds")
     when 5 
-      "fifth"
+      ChristmasDayFive.new(day_number: 5, gift: "golden rings")
     when 6
-      "sixth"
+      ChristmasDaySix.new(day_number: 6, gift: "geese a-laying")
     when 7 
-      "seventh"
+      ChristmasDaySeven.new(day_number: 7, gift: "swans a-swimming")
     when 8
-      "eighth"
+      ChristmasDayEight.new(day_number: 8, gift: "maids a-milking")
     when 9
-      "ninth"
+      ChristmasDayNine.new(day_number: 9, gift: "ladies dancing")
     when 10
-      "tenth"
+      ChristmasDayTen.new(day_number: 10, gift: "lords a-leaping")
     when 11
-      "eleventh"
-    when 12
-      "twelvth"
+      ChristmasDayEleven.new(day_number: 11, gift: "pipers piping")
+    else
+      ChristmasDayTwelve.new(day_number: 12, gift: "drummers drumming")
     end
   end
+end
 
-  def line(day)
-    case day
-    when 1
-      "A partridge in a pear tree."
-    when 2
-      "Two turtle doves, and"
-    when 3
-      "Three french hens"
-    when 4 
-      "Four calling birds"
-    when 5 
-      "Five golden rings"
-    when 6
-      "Six geese a-laying"
-    when 7 
-      "Seven swans a-swimming"
-    when 8
-      "Eight maids a-milking"
-    when 9
-      "Nine ladies dancing"
-    when 10
-      "Ten lords a-leaping"
-    when 11
-      "Eleven pipers piping"
-    when 12
-      "Twelve drummers drumming"
-    end
+class ChristmasDay
+  attr_reader :day_number
+  def initialize(day_number:, gift:)
+    @day_number = day_number
+    @gift = gift
+  end
+
+  def verse
+    "On the #{ordinate} day of Christmas my true love sent to me\n#{lines}\n" 
+  end
+
+  def lines
+    @day_number.downto(1).map{ |day| ChristmasDayFactory.for(day).line }.join("\n")
+  end
+
+  def ordinate
+    "first"
+  end
+
+  def line
+    "A #{@gift}."
+  end
+end
+
+class ChristmasDayTwo < ChristmasDay
+  def ordinate
+    "second"
+  end
+
+  def line
+    "Two #{@gift}, and"
+  end
+end
+
+class ChristmasDayThree < ChristmasDay
+  def ordinate
+    "third"
+  end
+
+  def line
+    "Three #{@gift}"
+  end
+end
+
+class ChristmasDayFour < ChristmasDay
+  def ordinate
+    "fourth"
+  end
+
+  def line
+    "Four #{@gift}"
+  end
+end
+
+class ChristmasDayFive < ChristmasDay
+  def ordinate
+    "fifth"
+  end
+
+  def line
+    "Five #{@gift}"
+  end
+end
+
+class ChristmasDaySix < ChristmasDay
+  def ordinate
+    "sixth"
+  end
+
+  def line
+    "Six #{@gift}"
+  end
+end
+
+class ChristmasDaySeven < ChristmasDay
+  def ordinate
+    "seventh"
+  end
+
+  def line
+    "Seven #{@gift}"
+  end
+end
+
+class ChristmasDayEight < ChristmasDay
+  def ordinate
+    "eighth"
+  end
+
+  def line
+    "Eight #{@gift}"
+  end
+end
+
+class ChristmasDayNine < ChristmasDay
+  def ordinate
+    "ninth"
+  end
+
+  def line
+    "Nine #{@gift}"
+  end
+end
+
+class ChristmasDayTen < ChristmasDay
+  def ordinate
+    "tenth"
+  end
+
+  def line
+    "Ten #{@gift}"
+  end
+end
+
+class ChristmasDayEleven < ChristmasDay
+  def ordinate
+    "eleventh"
+  end
+
+  def line
+    "Eleven #{@gift}"
+  end
+end
+
+class ChristmasDayTwelve < ChristmasDay
+  def ordinate
+    "twelvth"
+  end
+
+  def line
+    "Twelve #{@gift}"
   end
 end
--- test/twelve_days_test.rb
+++ test/twelve_days_test.rb
@@ -1,165 +1,44 @@
 gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'byebug'
 require_relative '../lib/twelve_days'
 
-class TwelveDaysTest < Minitest::Test
-  def test_verse_1
-    expected =
-      "On the first day of Christmas my true love sent to me\n" +
-      "A partridge in a pear tree.\n"
-    assert_equal expected, TwelveDays.new.verse(1)
+class VerseNumberFake
+  def initialize(number)
+    @number = number
   end
 
-  def test_verse_2
+  def verse
+    "Test verse #{@number}"
+  end
+end
+
+class VerseFactoryFake
+  def self.for(number)
+    VerseNumberFake.new(number)
+  end
+end
+
+class CountdownSongTest < Minitest::Test
+  def test_song
     expected =
-    "On the second day of Christmas my true love sent to me\n" +
-    "Two turtle doves, and\n" +
-    "A partridge in a pear tree.\n"
-    assert_equal expected, TwelveDays.new.verse(2)
+      "Test verse 96\n" +
+      "Test verse 97\n" +
+      "Test verse 98\n" +
+      "Test verse 99"
+    assert_equal(expected, CountdownSong.new(verse_factory: VerseFactoryFake, max: 99, min: 96).song)
   end
 
-  def test_verse_3
+  def test_verses
     expected =
-    "On the third day of Christmas my true love sent to me\n" +
-    "Three french hens\n" +
-    "Two turtle doves, and\n" +
-    "A partridge in a pear tree.\n"
-    assert_equal expected, TwelveDays.new.verse(3)
+      "Test verse 96\n" +
+      "Test verse 97"
+    assert_equal(expected, CountdownSong.new(verse_factory: VerseFactoryFake, max: 99, min: 96).verses(96, 97))
   end
 
-  def test_a_couple_verses
-    expected =
-    "On the first day of Christmas my true love sent to me\n" +
-    "A partridge in a pear tree.\n" +
-    "\n"+
-    "On the second day of Christmas my true love sent to me\n" +
-    "Two turtle doves, and\n" +
-    "A partridge in a pear tree.\n"
-    assert_equal expected, TwelveDays.new.verses(1, 2)
-  end
-
-  def test_a_few_verses
-    expected =
-    "On the first day of Christmas my true love sent to me\n" +
-    "A partridge in a pear tree.\n" +
-    "\n"+
-    "On the second day of Christmas my true love sent to me\n" +
-    "Two turtle doves, and\n" +
-    "A partridge in a pear tree.\n" +
-    "\n"+
-    "On the third day of Christmas my true love sent to me\n" +
-    "Three french hens\n" +
-    "Two turtle doves, and\n" +
-    "A partridge in a pear tree.\n"
-
-    assert_equal expected, TwelveDays.new.verses(1, 3)
-  end
-
-  def test_the_whole_song
-    expected = <<~SONG
-    On the first day of Christmas my true love sent to me
-    A partridge in a pear tree.
-
-    On the second day of Christmas my true love sent to me
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the third day of Christmas my true love sent to me
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the fourth day of Christmas my true love sent to me
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the fifth day of Christmas my true love sent to me
-    Five golden rings
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the sixth day of Christmas my true love sent to me
-    Six geese a-laying
-    Five golden rings
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the seventh day of Christmas my true love sent to me
-    Seven swans a-swimming
-    Six geese a-laying
-    Five golden rings
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the eighth day of Christmas my true love sent to me
-    Eight maids a-milking
-    Seven swans a-swimming
-    Six geese a-laying
-    Five golden rings
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the ninth day of Christmas my true love sent to me
-    Nine ladies dancing
-    Eight maids a-milking
-    Seven swans a-swimming
-    Six geese a-laying
-    Five golden rings
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the tenth day of Christmas my true love sent to me
-    Ten lords a-leaping
-    Nine ladies dancing
-    Eight maids a-milking
-    Seven swans a-swimming
-    Six geese a-laying
-    Five golden rings
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the eleventh day of Christmas my true love sent to me
-    Eleven pipers piping
-    Ten lords a-leaping
-    Nine ladies dancing
-    Eight maids a-milking
-    Seven swans a-swimming
-    Six geese a-laying
-    Five golden rings
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-
-    On the twelvth day of Christmas my true love sent to me
-    Twelve drummers drumming
-    Eleven pipers piping
-    Ten lords a-leaping
-    Nine ladies dancing
-    Eight maids a-milking
-    Seven swans a-swimming
-    Six geese a-laying
-    Five golden rings
-    Four calling birds
-    Three french hens
-    Two turtle doves, and
-    A partridge in a pear tree.
-    SONG
-    assert_equal expected, TwelveDays.new.song
+  def test_verse
+    expected = "Test verse 97"
+    assert_equal(expected, CountdownSong.new(verse_factory: VerseFactoryFake, max: 99, min: 96).verse(97))
   end
 end
